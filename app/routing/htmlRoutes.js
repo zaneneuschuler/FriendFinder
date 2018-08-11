4b6523be773e5ef1122d476c2cdcc8fefad79417
zane <neuschuler@gmail.com>
2018-08-11 16:52:28 -0700

    why doesn't this work on heroku

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,19 +3,15 @@ var path = require("path");
 module.exports = function (app) {
 
     app.get("/survey", function (req, res) {
-        res.sendFile(path.join(__dirname, "/public/survey.html"));
+        res.sendFile(path.join(__dirname, "public/survey.html"));
     });
     app.get("/style", function (req, res) {
-        res.sendFile(path.join(__dirname, "/public/style.css"));
+        res.sendFile(path.join(__dirname, "public/style.css"));
         console.log("css called");
-    });
-    app.get("/public/background.jpg", function (req, res) {
-        res.sendFile(path.join(__dirname, "/public/background.jpg"));
-        console.log("background called");
     });
     // If no matching route is found default to home
     app.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname, "/public/home.html"));
+        res.sendFile(path.join(__dirname, "public/home.html"));
     });
 
 };
