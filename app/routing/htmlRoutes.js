4f4bf04290a4de80879deacf1ea7de24627907c7
zane <neuschuler@gmail.com>
2018-08-11 16:24:22 -0700

    eh done

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -9,6 +9,10 @@ module.exports = function (app) {
         res.sendFile(path.join(__dirname, "../public/style.css"));
         console.log("css called");
     });
+    app.get("/public/background.jpg", function (req, res) {
+        res.sendFile(path.join(__dirname, "./public/background.jpg"));
+        console.log("background called");
+    });
     // If no matching route is found default to home
     app.get("*", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
