5e593d75cc55c054fc00fe296f5cf3e2f3c1616d
zane <neuschuler@gmail.com>
2018-08-11 17:07:15 -0700

    What

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,15 +3,15 @@ var path = require("path");
 module.exports = function (app) {
 
     app.get("/survey", function (req, res) {
-        res.sendFile(path.join(__dirname, "../../public/survey.html"));
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
     app.get("/style", function (req, res) {
-        res.sendFile(path.join(__dirname, "../../public/style.css"));
+        res.sendFile(path.join(__dirname, "../public/style.css"));
         console.log("css called");
     });
     // If no matching route is found default to home
     app.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname, "../../public/home.html"));
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 
 };
